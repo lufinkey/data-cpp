3f4697048617e11c42f1d1c39cd5839d5be891c6
Luis Finke <lfinke@fusionalliance.com>
2019-09-18 16:18:10 -0400

    add PlatformChecks.hpp

---
+++ src/fgl/data/PlatformChecks.hpp
@@ -0,0 +1,42 @@
+//
+//  PlatformChecks.hpp
+//  DataCpp
+//
+//  Created by Luis Finke on 9/18/19.
+//  Copyright © 2019 Luis Finke. All rights reserved.
+//
+
+#pragma once
+
+#ifndef _DATACPP_PLATFORMCHECKS_HPP
+#define _DATACPP_PLATFORMCHECKS_HPP
+
+#if defined(__APPLE__)
+#include "TargetConditionals.h"
+#endif
+
+#if defined(TARGET_IPHONE_SIMULATOR) && (TARGET_IPHONE_SIMULATOR == 1)
+	#define TARGETPLATFORM_IOS
+	#define TARGETPLATFORM_IOS_SIMULATOR
+#elif defined(TARGET_OS_IPHONE) && (TARGET_OS_IPHONE == 1)
+	#define TARGETPLATFORM_IOS
+	#define TARGETPLATFORM_IOS_DEVICE
+#elif defined (__ANDROID__)
+	#define TARGETPLATFORM_ANDROID
+#elif defined(_WIN32) || defined(__MINGW32__)
+	#define TARGETPLATFORM_WINDOWS
+#elif defined(TARGET_OS_MAC) && (TARGET_OS_MAC == 1)
+	#define TARGETPLATFORM_MAC
+#elif defined(__linux__) || defined(__linux)
+	#define TARGETPLATFORM_LINUX
+#elif defined(__unix__) || defined(__unix)
+	#define TARGETPLATFORM_UNIX
+#endif
+
+#if defined(TARGETPLATFORM_IOS) || defined(TARGETPLATFORM_ANDROID)
+	#define TARGETPLATFORMTYPE_MOBILE
+#elif defined(TARGETPLATFORM_WINDOWS) || defined(TARGETPLATFORM_MAC) || defined(TARGETPLATFORM_LINUX) || defined(TARGETPLATFORM_UNIX)
+	#define TARGETPLATFORMTYPE_DESKTOP
+#endif
+
+#endif
