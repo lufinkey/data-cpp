c75e472615a5863a7f3683fd19d5b430b6c94e85
Luis Finke <lfinke@fusionalliance.com>
2020-09-14 12:33:40 -0400

    attempt to get dependency compilation to work

--- projects/android-studio/app/src/main/cpp/native-lib.cpp
+++ projects/android-studio/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,13 @@
 #include <jni.h>
 #include <string>
+#include <fgl/data.hpp>
+
+using namespace fgl;
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_lufinkey_datacpp_MainActivity_stringFromJNI(
 		JNIEnv *env,
 		jobject /* this */) {
-	std::string hello = "Hello from C++";
-	return env->NewStringUTF(hello.c_str());
+	auto any = Any(String("hello world"));
+	return env->NewStringUTF(any.as<String>().c_str());
 }
