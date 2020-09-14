548e67c14df9184d38d8bfc3aa50b72d13d337d3
Luis Finke <lfinke@fusionalliance.com>
2020-09-14 12:42:12 -0400

    update android targetSdkVersion to 29 and add JNIDataCpp

---
+++ src/fgl/data/JNIDataCpp.hpp
@@ -0,0 +1,14 @@
+//
+//  JNIDataCpp.hpp
+//  DataCpp
+//
+//  Created by Luis Finke on 9/14/20.
+//  Copyright © 2020 Luis Finke. All rights reserved.
+//
+
+#pragma once
+
+#ifdef __ANDROID__
+#include <jni.h>
+
+#endif
