59df353b536ea9d83c63e2ffd7f1286bf5eef080
Luis Finke <lfinke@fusionalliance.com>
2021-07-01 21:37:18 -0400

    remove JNICALL

--- src/fgl/data/JNIDataCpp.cpp
+++ src/fgl/data/JNIDataCpp.cpp
@@ -10,7 +10,7 @@
 #include <fgl/data/JNIDataCpp.hpp>
 #include <android/log.h>
 
-JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
+JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
 	__android_log_print(ANDROID_LOG_DEBUG, "DataCpp", "JNI module initialized");
 	return JNI_VERSION_1_6;
 }
