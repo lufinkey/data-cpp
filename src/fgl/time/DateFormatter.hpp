b59db6f5fbbf07b948b19f12f0bf9a070ccce17c
Luis Finke <luisfinke@gmail.com>
2022-01-04 14:13:39 -0500

    add some documentation to DateFormatter

--- src/fgl/time/DateFormatter.hpp
+++ src/fgl/time/DateFormatter.hpp
@@ -14,16 +14,23 @@
 #include <fgl/data/String.hpp>
 
 namespace fgl {
+	/// Parses a string into a date, and formats a date into a string
 	class DateFormatter {
 	public:
+		/// The format of the string to be either read from or outputted
 		String format;
+		/// When parsing, this represents the time zone of the date string (this is overridden if the string provides a time zone)
+		/// When formatting, this represents the time zone the output string should be displayed in
 		TimeZone timeZone = TimeZone::currentAlways();
+		/// The locale of the date string
 		std::locale locale;
 		
+		/// Formats a date string for the given date
 		String stringFromDate(const Date& date) const;
+		/// Parses a date from the given string. Returns `std::nullopt` if a valid date could not be parsed
 		Optional<Date> dateFromString(String dateString) const;
 	};
 
-
+	/// Parses an ISO8601 duration string into a time interval
 	TimeInterval parseISO8601Duration(const String& durationString);
 }
